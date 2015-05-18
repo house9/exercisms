cca3cbb2b8c79ba2f1ca7852ab2b05000187fee3
Jesse House <mail@jessehouse.com>
2015-05-18 09:20:23 -0700

    Refactor: elixir-bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,14 +1,27 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-      String.strip(input) == "" ->
+      silent?(input) ->
         "Fine. Be that way!"
-      String.ends_with?(input, "?") ->
+      question?(input) ->
         "Sure."
-      String.upcase(String.replace(input, ~r/\P{L}/, "")) == String.replace(input, ~r/\P{L}/, "") && String.length(String.replace(input, ~r/\P{L}/, "")) > 0 ->
+      yelling?(input) ->
         "Whoa, chill out!"
       true ->
         "Whatever."
     end
   end
+
+  defp silent?(input) do
+    String.strip(input) == ""
+  end
+
+  defp question?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp yelling?(input) do
+    String.upcase(String.replace(input, ~r/\P{L}/, "")) == String.replace(input, ~r/\P{L}/, "") && String.length(String.replace(input, ~r/\P{L}/, "")) > 0
+  end
+
 end
