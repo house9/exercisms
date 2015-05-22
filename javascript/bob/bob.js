a3bde03164935fa28624849a00f359aecbc20ea6
Jesse House <mail@jessehouse.com>
2015-05-22 10:19:16 -0700

    javascript: implement bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,28 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  var silent = function (text) {
+    return text.trim().length === 0;
+  };
+
+  var question = function (text) {
+    return text.substring(text.length - 1) === "?";
+  };
+
+  var yelling = function (text) {
+    var normalized = text.replace(/[^\u00C0-\u017Fa-zA-Z]/g, "");
+    return normalized.toUpperCase() === normalized && normalized.length > 0;
+  };
+
+  if (silent(input)) {
+    return "Fine. Be that way!";
+  } else if (yelling(input)) {
+    return "Whoa, chill out!";
+  } else if (question(input)) {
+    return "Sure.";
+  } else {
+    return "Whatever.";
+  }
 };
 
 module.exports = Bob;
