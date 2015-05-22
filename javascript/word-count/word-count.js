2c14a25d009ae483d0c8b22d9fd742cc379cc2c7
Jesse House <mail@jessehouse.com>
2015-05-22 10:55:17 -0700

    javascript: implement word-count

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -3,22 +3,29 @@ module.exports = function (sentance) {
     return word.length > 0;
   }
 
-  var initialize = function (result, words) {
+  var assignZero = function (result, word) {
+    result[word] = 0;
+  }
+
+  var addOne = function (result, word) {
+    result[word] += 1;
+  }
+
+  var iteration = function (result, words, func) {
     words.forEach(function (word) {
       if (isValid(word)) {
-        result[word] = 0;
+        func(result, word);
       }
     });
     return result;
   };
 
+  var initialize = function (result, words) {
+    return iteration(result, words, assignZero);
+  };
+
   var aggregate = function (result, words) {
-    words.forEach(function (word) {
-      if (isValid(word)) {
-        result[word] += 1;
-      }
-    });
-    return result;
+    return iteration(result, words, addOne);
   };
 
   var words = sentance.split(/\s/);
